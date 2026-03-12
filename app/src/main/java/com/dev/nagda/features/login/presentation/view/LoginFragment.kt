387bfecfb2682c78d6f6e37071185d8bc79be406
mohamed toba <mohamedtowba298@gmail.com>
2026-03-12 11:51:06 +0200

    add home ui

--- app/src/main/java/com/dev/nagda/features/login/presentation/view/LoginFragment.kt
+++ app/src/main/java/com/dev/nagda/features/login/presentation/view/LoginFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
@@ -83,11 +84,12 @@ class LoginFragment : Fragment() {
         }
 
         binding.loginButton.setOnClickListener{
-            val email = binding.emailEt.text.toString().trim()
-            val password = binding.passwordEt.text.toString().trim()
-            if (validateInputs(email, password)) {
+            navigateToHome()
+//            val email = binding.emailEt.text.toString().trim()
+//            val password = binding.passwordEt.text.toString().trim()
+//            if (validateInputs(email, password)) {
 //                viewModel.login(email, password)
-            }
+//            }
         }
     }
 
